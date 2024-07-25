07ddc1634217e788bc9ef22258cc4f040e24b2bf
woolfie59 <woolford.sophie@gmail.com>
2024-07-25 20:09:25 +1000

    added login route to auth controller

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,9 +1,12 @@
+from datetime import timedelta
+
 from flask import Blueprint, request
 
 from init import bcrypt, db
 from models.user import User, user_schema
 from sqlalchemy.exc import IntegrityError
 from psycopg2 import errorcodes
+from flask_jwt_extended import create_access_token
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
@@ -37,3 +40,22 @@ def register_user():
             return {"error": f"The column {err.orig.diag.column_name} is required"}, 409
         if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
             return {"error": "Email address already in use"}, 409
+        
+
+@auth_bp.route("login", methods=["POST"])
+def login_user():
+    # get data from body of request
+    body_data = request.get_json()
+    # find user in db with that email address
+    stmt = db.select(User).filter_by(email=body_data.get("email"))
+    user = db.session.scalar(stmt)
+    # if user exists and passowrd is correct
+    if user and bcrypt.check_password_hash(user.password, body_data.get("password")):
+        # create jwt
+        token = create_access_token(identity=str(user.id), expires_delta=timedelta(days=1))
+        # respond back
+        return {"email": user.email, "is_admin": user.is_admin, "token": token}
+    # else
+    else:
+        # respond back with error msg
+        return {"error": "Invalid email or password"}, 401
